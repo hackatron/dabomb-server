22c3db0b06e814bb7178a0a3b0bdc5c10a2b9247
Matteo Centenaro <bugant@gmail.com>
2012-07-21 15:19:03 +0200

    FIX: Pusher configuration does not depend on the environment

--- config/pusher_config.rb
+++ config/pusher_config.rb
@@ -1,7 +1,6 @@
 dir = File.expand_path(File.dirname(__FILE__))
 pusher_config = File.new(dir + "/pusher.yml").read
-env = (ENV["RACK_ENV"] || "development")
-pusher_settings = YAML::load(pusher_config)[env]
+pusher_settings = YAML::load(pusher_config)
 
 Pusher.app_id = pusher_settings['app_id']
 Pusher.key    = pusher_settings['key']
