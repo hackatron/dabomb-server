c8d2b18c2667ef53c19428263fa0f1dcbdb11ba4
Giovanni Cappellotto <potomak84@gmail.com>
2012-07-21 17:13:09 +0200

    Add production configuration for Pusher

--- config/pusher_config.rb
+++ config/pusher_config.rb
@@ -1,6 +1,14 @@
-dir = File.expand_path(File.dirname(__FILE__))
-pusher_config = File.new(dir + "/pusher.yml").read
-pusher_settings = YAML::load(pusher_config)
+if 'production' == ENV['RACK_ENV']
+  pusher_settings = {
+    'app_id' => ENV['PUSHER_API_ID'],
+    'key'    => ENV['PUSHER_KEY'],
+    'secret' => ENV['PUSHER_SECRET']
+  }
+else
+  dir = File.expand_path(File.dirname(__FILE__))
+  pusher_config = File.new(dir + "/pusher.yml").read
+  pusher_settings = YAML::load(pusher_config)
+end
 
 Pusher.app_id = pusher_settings['app_id']
 Pusher.key    = pusher_settings['key']
