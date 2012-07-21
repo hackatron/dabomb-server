87109cae8017147499824ba540cf7bf7d48ad5b5
Matteo Centenaro <bugant@gmail.com>
2012-07-21 14:13:47 +0200

    Add dabomb API

--- da_bomb.rb
+++ da_bomb.rb
@@ -1,10 +1,35 @@
 class DaBomb < Sinatra::Base
+  before do
+    content_type 'application/json'
+  end
+
   post '/player' do
-    if params[:username].blank?
+    player = Player.new({:username => Player.next})
+    player.register
+
+    status 200
+    return Yajl::Encoder.encode({:username => player.username})
+  end
+
+  post '/play/:username' do |username|
+    player = Player.new({:username => username})
+
+    status 200
+    return Yajl::Encoder.encode({:code => player.pair})
+  end
+
+  post '/score/:username' do |username|
+    code = params[:code]
+    score = params[:score]
+
+    if code.blank? || score.blank?
       status 400
-      return Yajl::Encoder.encode({:error => "Please, provide a username"})
+      return Yajl::Encoder.encode({:error => 'Please, provide the match code and the player score', :error_code => 'E.02'})
     end
 
-    player = Player.new({:username => params[:username]})
+    match = Match.from_code(code)
+    match.score(username, score)
+
+    status 204
   end
 end
