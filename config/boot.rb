ad245ff9f5bd206fd986ef2b7f0edb2c50281716
Matteo Centenaro <bugant@gmail.com>
2012-07-21 13:52:50 +0200

    Load BombStore module stuff

--- config/boot.rb
+++ config/boot.rb
@@ -8,4 +8,8 @@ Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 $:.unshift(File.join(File.dirname(__FILE__), '/../'))
 
 require 'logger'
+
+require 'lib/bomb_store/redis'
+require 'lib/bomb_store/connection'
+
 require 'da_bomb'
