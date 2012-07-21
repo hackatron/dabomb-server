3c15aed79a337207a8b775e4cac26638c58f1ffb
Matteo Centenaro <bugant@gmail.com>
2012-07-21 15:05:52 +0200

    Configure connections
    Add config file for Pusher and Redis

--- config/pusher_config.rb
+++ config/pusher_config.rb
@@ -1,4 +1,9 @@
-Pusher.app_id = GowarParam::PUSHER_APP_ID
-Pusher.key    = GowarParam::PUSHER_KEY
-Pusher.secret = GowarParam::PUSHER_SECRET
+dir = File.expand_path(File.dirname(__FILE__))
+pusher_config = File.new(dir + "/pusher.yml").read
+env = (ENV["RACK_ENV"] || "development")
+pusher_settings = YAML::load(pusher_config)[env]
+
+Pusher.app_id = pusher_settings['app_id']
+Pusher.key    = pusher_settings['key']
+Pusher.secret = pusher_settings['secret']
 Pusher.logger = $logger if $logger
